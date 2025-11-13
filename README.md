d6b0b47e53bde3cc1ab8e55882203f9d6d9a7c37
manny <onwukasomtochukwu2022@gmail.com>
2025-11-14 00:45:27 +0100

    `Updated Tailwind CSS configuration and added Firebase authentication to the signup page`

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # To Start the Tailwind CLI build process
 npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch
 
-# To Run Locally
+# To Run Locallyb
 - npx serve
 - install serve@14.2.5 package (By accepting)
 - Choose src as the root directory
---
+++ src/js/signup.js
@@ -0,0 +1,46 @@
+// const firebaseapp = firebase.initializeApp({
+//   apiKey: "AIzaSyBo_JSPeazqkYzNbTiO9Cx3mIDbhcBUYgo",
+//   authDomain: "fir-frontend-cedb2.firebaseapp.com",
+//   projectId: "fir-frontend-cedb2",
+//   storageBucket: "fir-frontend-cedb2.appspot.com",
+//   messagingSenderId: "735535365900",
+//   appId: "1:735535365900:web:6712a63f27c03f80151329",
+//   measurementId: "G-KJ2HY43NDE",
+// });
+
+// const auth = firebaseapp.auth();
+
+// function register() {
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+
+//   auth
+//     .createUserWithEmailAndPassword(email, password)
+//     .then((res) => {
+//       console.log("✅ User created:", res.user);
+//       alert("Account created successfully!");
+//     })
+//     .catch((err) => {
+//       console.error("❌ Error:", err.message);
+//       alert(err.message);
+//     });
+// }
+
+// const myFn = fetch(myFn()).then((tru, fal) => {
+//   x == 0;
+//   X == 0 ? tru("OK") : fal("Error");
+// });
+// console.log(myFn);
+
+async function getData() {
+  try{
+    const res = await fetch('https://randomuser.me/api/')
+    const data = await res.json();
+    console.group(data.results[0].cell)
+  }catch(err){
+    console.log({err})
+  }
+}
+getData()
+// const promise = fetch("https://api.example.com/data");
+// console.log(promise); 
