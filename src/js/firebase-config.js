f7ffd761ea6f8abd8dde1f6bfaad2489b55cf847
manny <onwukasomtochukwu2022@gmail.com>
2025-11-20 00:34:03 +0100

    `Removed .gitignore file and hardcoded Firebase config in favor of importing from separate file`

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,8 +1,7 @@
 // This file should be added to your .gitignore
-
 export const firebaseConfig = {
-  apiKey: "import.meta.env.PUBLIC_FIREBASE_API_KEY", // It's still recommended to restrict this key in the Google Cloud Console
-  authDomain: "PUBLIC_FIREBASE_AUTH_DOMAIN",
+  apiKey: "AIzaSyAokU-izWHCMnfl4I0ZuTZR2AQCh6dUDEk", // It's still recommended to restrict this key in the Google Cloud Console
+  authDomain: "m4u-demo.firebaseapp.com",
   projectId: "m4u-demo",
   storageBucket: "m4u-demo.firebasestorage.app",
   messagingSenderId: "1043527984472",
