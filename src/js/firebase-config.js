ef16bbfc23bdf51541a99791334db36bb82b7ece
manny <onwukasomtochukwu2022@gmail.com>
2025-11-19 21:17:34 +0100

    Add firebase-config.js to the project with configuration details

---
+++ src/js/firebase-config.js
@@ -0,0 +1,9 @@
+// This file should be added to your .gitignore
+export const firebaseConfig = {
+  apiKey: "AIzaSyAokU-izWHCMnfl4I0ZuTZR2AQCh6dUDEk", // It's still recommended to restrict this key in the Google Cloud Console
+  authDomain: "m4u-demo.firebaseapp.com",
+  projectId: "m4u-demo",
+  storageBucket: "m4u-demo.firebasestorage.app",
+  messagingSenderId: "1043527984472",
+  appId: "1:1043527984472:web:9e67796046c6086e79712d",
+};
