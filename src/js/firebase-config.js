e37e6a17a1057355f7ba59f27e4aaa7e50e68b59
manny <onwukasomtochukwu2022@gmail.com>
2025-11-19 23:37:26 +0100

    `Update .gitignore and firebase-config.js to ignore dependencies and build output`

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,7 +1,8 @@
 // This file should be added to your .gitignore
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyAokU-izWHCMnfl4I0ZuTZR2AQCh6dUDEk", // It's still recommended to restrict this key in the Google Cloud Console
-  authDomain: "m4u-demo.firebaseapp.com",
+  apiKey: "import.meta.env.PUBLIC_FIREBASE_API_KEY", // It's still recommended to restrict this key in the Google Cloud Console
+  authDomain: "PUBLIC_FIREBASE_AUTH_DOMAIN",
   projectId: "m4u-demo",
   storageBucket: "m4u-demo.firebasestorage.app",
   messagingSenderId: "1043527984472",
